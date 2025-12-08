c0994bdf4ead2bce3582edbc0a932f66a62a39f0
Remy4657 <88965436+Remy4657@users.noreply.github.com>
2025-12-08 07:23:49 +0700

    mapper dto

--- src/main/java/com/example/demo/dto/ProductResponse.java
+++ src/main/java/com/example/demo/dto/ProductResponse.java
@@ -6,23 +6,28 @@ import lombok.AllArgsConstructor;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductResponse {
     private Long id;
+    @JsonProperty("title")
     private String name;
     private String description;
+    @JsonProperty("price")
 
     private Double priceOriginal;
     private Integer discountPercent;
     private Double salePrice; // computed
-
+    @JsonProperty("thumbnail")
     private String thumb; // thumbnail (position=0)
+    @JsonProperty("hoverThumbnail")
     private String thumbHover; // hover image (position=1)
 
-    private CategoryResponse category;
-
+    // private CategoryResponse category;
+    private String pcate;
     private List<ProductImageResponse> images;
     private List<SizeResponse> sizes;
     private List<ColorResponse> colors;
--- src/main/java/com/example/demo/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ProductServiceImpl.java
@@ -4,23 +4,29 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import com.example.demo.mapper.ProductMapper;
+import com.example.demo.dto.ProductResponse;
 import com.example.demo.entity.product.ProductEntity;
 import com.example.demo.repository.ProductRepository;
 import com.example.demo.service.ProductService;
 
-@Service
-public class ProductServiceImpl implements ProductService {
-    @Autowired
-    private ProductRepository productRepository;
+import lombok.RequiredArgsConstructor;
 
+@Service
+@RequiredArgsConstructor
+public class ProductServiceImpl implements ProductService {
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
     // @Override
     // public ProductEntity createProduct(ProductEntity product) {
     // return productRepository.createProduct(product);
     // }
 
     @Override
-    public List<ProductEntity> findAllProducts() {
-        return productRepository.findAll();
+    public List<ProductResponse> findAllProducts() {
+        return productRepository.findAll().stream()
+                .map(productMapper::toProductResponse)
+                .toList();
+
     }
 }
